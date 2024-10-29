26906829399a59d5b3d249bbaeaa1f48f4ab94e1
sonsumin <sonsumin00@gmail.com>
2024-10-29 23:25:15 +0900

    Feat: "first commit"

---
+++ src/main/java/univ/yesummit/YesummitApplication.java
@@ -0,0 +1,13 @@
+package univ.yesummit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesummitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesummitApplication.class, args);
+	}
+
+}
