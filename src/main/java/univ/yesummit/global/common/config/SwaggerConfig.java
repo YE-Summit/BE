b4112d2cb4a284dacfe51af9ba8ed2fb33e88403
sonsumin <sonsumin00@gmail.com>
2024-11-01 15:47:36 +0900

    Feat: 스웨거 추가

---
+++ src/main/java/univ/yesummit/global/common/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package univ.yesummit.global.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .version("v1.0.0")              // 버전 기록
+                        .title("YE;Summit API 명세서")     // API 명세서 제목
+                        .description("API 명세서"));      // 상세
+
+    }
+}
